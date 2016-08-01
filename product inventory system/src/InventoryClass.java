04c9c7caaa9941c630b7d50851aa1f1ad70c77ff
jeffin.Jose <jeffin.Jose@B107-04.soit.local>
2016-08-02 09:44:39 +1200

    Function called getdetails created

--- product inventory system/src/InventoryClass.java
+++ product inventory system/src/InventoryClass.java
@@ -1,4 +1,23 @@
 
+import java.util.*;
+
 public class InventoryClass {
+	 String T_name;
+	 int P_Id;
+	    String P_Name;
+	    String P_Description;
+	    String P_Quantity;
+	    String P_MngDate;
+	    int P_price;
+	    
+	    public static void main(String[] args){
+	    	new InventoryClass().Getdetails();
+	    	
+	    }
+	    public void Getdetails()
+	    {
+	    	
+	    	
+	    }
 
 }
